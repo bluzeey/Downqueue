feefcab6ea6db7399e8fc3611219596f5d98c4cc
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-12 20:31:58 +0530

    changing words of the website

--- src/containers/heroimg.js
+++ src/containers/heroimg.js
@@ -5,9 +5,9 @@
      return(
          <HeroImg>
              <HeroImg.Inner>
-             <HeroImg.Title>Make Booking Easier!</HeroImg.Title>
+             <HeroImg.Title>Meet New People!</HeroImg.Title>
              <HeroImg.Paragraph>
-                 Organize schedule, appointments at your own convenience!
+                 Organize schedule, appointments to meet new strangers!
              </HeroImg.Paragraph>
              <HeroImg.Button>Get Started &#10142;</HeroImg.Button>
              </HeroImg.Inner>
