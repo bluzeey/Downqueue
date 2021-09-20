5d7235344ef73ec08c6fc5d0035907110db9333e
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-20 15:44:44 +0530

    Changing responsiveness of the website

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -8,7 +8,7 @@ export default function Cards(){
         {CardsData.map((Card)=>(
             <div className={`cards-Inner ${Card.style}`}>
             <h3 className="cards-Title">{Card.title}</h3>
-            <div style={{display:"flex",alignItems:"center"}}>
+            <div className="cards-Content">
             <img className="cards-img" src={Card.img} alt="Schedule"/>
             <p className="cards-Paragraph">{Card.text}</p>
             </div>
