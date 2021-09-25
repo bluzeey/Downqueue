75cdf54f6f853ed48fc2cd31bf52996a4f7f4950
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-25 11:33:35 +0530

    Making a client side and shifting the files

---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,9 @@
+import React,{useState} from 'react'
+import Calendar from '../components/calendar/Calendar';
+const Dashboard = () => {
+    return (
+            <Calendar/>
+    )
+}
+
+export default Dashboard
