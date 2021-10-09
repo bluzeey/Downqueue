82539043d22db9351005a9c58f8597c1737bc667
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-09 14:09:01 +0530

    Adding .env variables

--- client/src/lib/firebase.prod.js
+++ client/src/lib/firebase.prod.js
@@ -6,12 +6,12 @@ import 'firebase/compat/auth';
 //import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyDlszAPcnDUy3cLwUJHx6LRKCHiRcsJs8Y",
-  authDomain: "downqueue-8b11d.firebaseapp.com",
-  projectId: "downqueue-8b11d",
-  storageBucket: "downqueue-8b11d.appspot.com",
-  messagingSenderId: "132972018277",
-  appId: "1:132972018277:web:259247bff30f5b5806a48c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const firebase = Firebase.initializeApp(config);
