e5add01f4475cfe9402b634600928c2a0b85e9e3
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-10 16:01:43 +0530

    updating stuff

--- client/src/pages/meet.js
+++ client/src/pages/meet.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import {StreamChat} from 'stream-chat';
+import {StreamChat,connect} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import ChannelListContainer from '../components/ChannelListContainer'
 import ChannelContainer from '../components/ChannelContainer'
 
-const apiKey=`${process.env.REACT_APP_STREAM_API_KEY}`;
-const apiSecret=process.env.REACT_APP_STREAM_API_SECRET
-const appId=process.env.REACT_APP_STREAM_API_APP_ID
+const user=JSON.parse(window.localStorage.getItem('authUser'))
+const apiKey=process.env.REACT_APP_STREAM_API_KEY;
+const apiSecret=process.env.REACT_APP_STREAM_API_SECRET;
+const appId=process.env.REACT_APP_STREAM_API_APP_ID;
 const client= StreamChat.getInstance(apiKey);
+const ConnectUser=async(user)=>{
+    await client.connectUser(
+        {id:user.id,
+        name:user.name}
+    )
+}
+
 const Meet = () => {
     return (
         <div>
