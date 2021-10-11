69f6944cb2eeb400e825869ea74afe7612f80f98
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-11 09:25:22 +0530

    Creating the chat groundup

--- client/src/pages/meet.js
+++ client/src/pages/meet.js
@@ -1,31 +1,108 @@
-import React from 'react'
-import {StreamChat,connect} from 'stream-chat';
-import {Chat} from 'stream-chat-react';
-import Cookies from 'universal-cookie';
-import ChannelListContainer from '../components/ChannelListContainer'
-import ChannelContainer from '../components/ChannelContainer'
+import React, { useEffect, useState,useContext } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  LoadingIndicator,
+  MessageInput,
+  MessageList,
+  Thread,
+  useMessageContext,
+  Window,
+} from 'stream-chat-react';
+import { useAuthListener } from '../hooks';
+import { FirebaseContext } from '../context/firebase';
+import 'stream-chat-react/dist/css/index.css';
 
-const user=JSON.parse(window.localStorage.getItem('authUser'))
-const apiKey=process.env.REACT_APP_STREAM_API_KEY;
-const apiSecret=process.env.REACT_APP_STREAM_API_SECRET;
-const appId=process.env.REACT_APP_STREAM_API_APP_ID;
-const client= StreamChat.getInstance(apiKey);
-const ConnectUser=async(user)=>{
-    await client.connectUser(
-        {id:user.id,
-        name:user.name}
-    )
-}
+
+server_client = stream_chat.StreamChat(api_key="STREAM_KEY", api_secret="STREAM_SECRET") 
+token = server_client.create_token('john')
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiaWN5LWZvZy0yIn0.jkEQ-J1EkpTJv104D_KIC_tWLLdDPCZjDqahfTR6Q5A';
+const filters = { type: 'messaging', members: { $in: ['icy-fog-2'] } };
+const sort = { last_message_at: -1 };
+
+const CustomChannelPreview = (props) => {
+  const { channel, setActiveChannel } = props;
+
+  const { messages } = channel.state;
+  const messagePreview = messages[messages.length - 1]?.text.slice(0, 30);
+
+  return (
+    <div onClick={() => setActiveChannel(channel)} style={{ margin: '12px' }}>
+      <div>{channel.data.name || 'Unnamed Channel'}</div>
+      <div style={{ fontSize: '14px' }}>{messagePreview}</div>
+    </div>
+  );
+};
+
+const CustomMessage = () => {
+  const { message } = useMessageContext();
+
+  return (
+    <div>
+      <b style={{ marginRight: '4px' }}>{message.user.name}</b> {message.text}
+    </div>
+  );
+};
 
 const Meet = () => {
-    return (
-        <div>
-            <Chat client={client} theme="team light">
-                <ChannelListContainer/>
-                <ChannelContainer/>
-            </Chat>
-        </div>
-    )
-}
+  const [chatClient, setChatClient] = useState(null);
+    const { user } = useAuthListener();
+    const{ firebase }=useContext(FirebaseContext)
+    const [profileData, setProfileData] = useState({
+        Fullname:'',city:'',country:'',emailAddress:'',Bio:'',Tag:'',setProfile:false
+    })
+    const isInvalid=profileData.Fullname===""
+    const db=firebase.firestore();   
+    useEffect(()=>{
+       db.collection("Users-data").doc(user.uid).get().then(doc=>setProfileData({
+            Fullname:doc.data().Name,
+            city:doc.data().City,
+            country:doc.data().Country,
+            emailAddress:doc.data().emailAddress,
+            Bio:doc.data().Bio,
+            Tag:doc.data().Tag,
+            setProfile:doc.data().setProfile})
+       )},[])
 
-export default Meet
+  useEffect(() => {
+    const initChat = async () => {
+      const client = StreamChat.getInstance('46pt5uptgfnd');
+
+      await client.connectUser(
+        {
+          id: user.uid,
+          name: profileData.name,
+          image: 'https://getstream.io/random_png/?id=icy-fog-2&name=icy-fog-2',
+        },
+        userToken,
+      );
+
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  if (!chatClient) {
+    return <LoadingIndicator />;
+  }
+
+  return (
+    <Chat client={chatClient} theme='messaging light'>
+      <ChannelList filters={filters} sort={sort} Preview={CustomChannelPreview} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList Message={CustomMessage} />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default Meet;
